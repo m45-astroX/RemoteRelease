70637f2856d9a6e48212845891d509a7b21e9898
m45-astroX <100989682+m45-astroX@users.noreply.github.com>
2022-04-21 12:18:32 +0900

    22.04.21_12.18

--- sketch_RemoteRelease_Sub/sketch_RemoteRelease_Sub.ino
+++ sketch_RemoteRelease_Sub/sketch_RemoteRelease_Sub.ino
@@ -1,10 +1,11 @@
 /*
-  Lantan Slave
-*/
 
-/*
-  I2C Slave
-
+  RemoteRelease sub
+  
+  ~ For Public ~
+  
+  m45-astroX
+  
   D0 : RX
   D1 : TX
   D2 : releaseSw
@@ -16,19 +17,18 @@
   D8 : RotarySw4
   D9 : RotarySw5
 
-
 */
 
 
-volatile byte ss = 'p';
+byte ss = 'p';
 int releaseStatus = LOW;
 int resetStatus = LOW;
-volatile int rotaryStatus1 = LOW;
-volatile int rotaryStatus2 = LOW;
-volatile int rotaryStatus3 = LOW;
-volatile int rotaryStatus4 = LOW;
-volatile int rotaryStatus5 = LOW;
-volatile int rotaryStatus6 = LOW;
+int rotaryStatus1 = LOW;
+int rotaryStatus2 = LOW;
+int rotaryStatus3 = LOW;
+int rotaryStatus4 = LOW;
+int rotaryStatus5 = LOW;
+int rotaryStatus6 = LOW;
 
 const int lantanPin = 4;
 const int rotarySw1 = 5;
@@ -41,7 +41,7 @@ const int rotarySw6 = 10;
 const int soundPin = 19;
 int soundStatus = 0;
 
-// ver for bright
+// var for bright
 int startT = 0;
 int waitT = 0;
 int nowT = 0;
@@ -69,14 +69,13 @@ long LDT2 = 0;
 const int releaseInPin = 10;
 int LBS3 = LOW;
 long LDT3 = 0;
-volatile int releaseInStatus = 0;
+int releaseInStatus = 0;
 
 int v = 0;
-
 int extra = 0;
 
+// actuator
 const int LEDPin = 11;
-
 int lastLEDT = 0;
 int lastSoundT = 0;
 
@@ -86,12 +85,10 @@ int lastDoubleSendlT = 0;
 int doubleSendD = 0;
 int lastDoubleSendDT = 0;
 
-int now6 = 0;
-
 int offPermitState = LOW;
 int releaseStartT = 0;
 
-
+/* setup */
 void setup() {
 
   Serial.begin(9600);
@@ -104,13 +101,14 @@ void setup() {
   pinMode(rotarySw4, INPUT_PULLUP);
   pinMode(rotarySw5, INPUT_PULLUP);
   pinMode(rotarySw6, INPUT_PULLUP);
-
+  
   pinMode(LEDPin, OUTPUT);
   pinMode(releaseInPin, INPUT);
-
   pinMode(soundPin, OUTPUT);
   digitalWrite(soundPin, HIGH);
+  
   lastSoundT = 0;
+  
 }
 
 void loop() {
@@ -122,30 +120,30 @@ void loop() {
 
   // release
   int releaseSelect = !digitalRead(rotarySw6);
-  if (releaseSelect == LOW) {
+  if ( releaseSelect == LOW ) {
     
     int releaseInStatus = !digitalRead(releaseInPin);
-    if (LBS3 == LOW && releaseInStatus == HIGH) {
-      int now6 = millis();
-      if ((now6 - LDT3) > DD) {
+    
+    if ( LBS3 == LOW && releaseInStatus == HIGH ) {
+      if (( millis() - LDT3 ) > DD) {
         Serial.print('l');
       }
-      LDT3 = now6;
+      LDT3 = millis();
     }
-    else if (LBS3 == HIGH && releaseInStatus == LOW) {
+    else if ( LBS3 == HIGH && releaseInStatus == LOW ) {
       Serial.print('D');
     }
     LBS3 = releaseInStatus;
   }
 
-  else if (releaseSelect == HIGH) { // auto mode
+  else if ( releaseSelect == HIGH ) {   // auto mode
     int releaseInStatus = !digitalRead(releaseInPin);
-    if (LBS3 == LOW && releaseInStatus == HIGH) {
+    if ( LBS3 == LOW && releaseInStatus == HIGH ) {
       Serial.print('l');
       doubleSendl = 5;
       lastDoubleSendlT = millis();
     }
-    else if (LBS3 == HIGH && releaseInStatus == LOW) {
+    else if ( LBS3 == HIGH && releaseInStatus == LOW ) {
       Serial.print('D');
       doubleSendD = 5;
       lastDoubleSendDT = millis();
@@ -153,162 +151,81 @@ void loop() {
     LBS3 = releaseInStatus;
   }
 
-  if (Serial.available() > 0) {
+  if ( Serial.available() > 0 ) {
     ss = Serial.read();
-    if (ss == 'Z') { 
+    if ( ss == 'Z' ) { 
       waitT = 0;
       ss = 'p';
       extra = 0;
       offRun();
       doubleSendD = 0;
     }
-    else if (ss == 'q') {
+    else if ( ss == 'q' ) {
       digitalWrite(LEDPin, HIGH);
       lastLEDT = millis();
       Serial.print('q');
     }
-    else if (ss == 'r') {
+    else if ( ss == 'r' ) {
       digitalWrite(soundPin, HIGH);
       lastSoundT = millis();
     }
     else {
-      if (ss == 'A') {
-        waitT = 30000;
-        extra = 1;
-      }
-      else if (ss == 'B') {
-        waitT = 20000;
-        extra = 1;
-      }
-      else if (ss == 'C') {
-        waitT = 10000;
-        extra = 1;
-      }
-      else if (ss == 'D') {
-        waitT = 30000;
-      }
-      else if (ss == 'E') {
-        waitT = 25000;
-      }
-      else if (ss == 'F') {
-        waitT = 20000;
-      }
-      else if (ss == 'G') {
-        waitT = 15000;
-      }
-      else if (ss == 'H') {
-        waitT = 13000;
-      }
-      else if (ss == 'I') {
-        waitT = 10000;
-      }
-      else if (ss == 'J') {
-        waitT = 8000;
-      }
-      else if (ss == 'K') {
-        waitT = 6000;
-      }
-      else if (ss == 'L') {
-        waitT = 5000;
-      }
-      else if (ss == 'M') {
-        waitT = 4000;
-      }
-      else if (ss == 'N') {
-        waitT = 3200;
-      }
-      else if (ss == 'O') {
-        waitT = 2500;
-      }
-      else if (ss == 'P') {
-        waitT = 2000;
-      }
-      else if (ss == 'Q') {
-        waitT = 1600;
-      }
-      else if (ss == 'R') {
-        waitT = 1300;
-      }
-      else if (ss == 'S') {
-        waitT = 1000;
-      }
-      else if (ss == 'T') {
-        waitT = 800;
-      }
-      else if (ss == 'U') {
-        waitT = 600;
-      }
-      else if (ss == 'V') {
-        waitT = 500;
-      }
-      else if (ss == 'W') {
-        waitT = 400;
-      }
-      else if (ss == 'X') {
-        waitT = 333;
-      }
-      else if (ss == 'Y') {
-        waitT = 250;
-      }
-      else if (ss == 'a') {
-        waitT = 200;
-      }
-      else if (ss == 'b') {
-        waitT = 167;
-      }
-      else if (ss == 'c') {
-        waitT = 125;
-      }
-      else if (ss == 'd') {
-        waitT = 100;
-      }
-      else if (ss == 'e') {
-        waitT = 77;
-      }
-      else if (ss == 'f') {
-        waitT = 67;
-      }
-      else if (ss == 'g') {
-        waitT = 50;
-      }
-      else if (ss == 'h') {
-        waitT = 40;
-      }
-      else if (ss == 'i') {
-        waitT = 33;
-      }
-      else if (ss == 'j') {
-        waitT = 25;
-      }
-      else if (ss == 'k') {
-        waitT = 20;
-      }
-      else if (ss == 'l') {
-        waitT = 17;
-      }
-      else if (ss == 'm') {
-        waitT = 13;
-      }
-      else if (ss == 'n') {
-        waitT = 10;
-      }
-      else if (ss == 'o') {
-        waitT = 8;
-      }
+      if ( ss == 'A' ) { waitT = 30000; extra = 1; }
+      else if (ss == 'B') { waitT = 20000; extra = 1; }
+      else if (ss == 'C') { waitT = 10000; extra = 1; }
+      else if (ss == 'D') { waitT = 30000; }
+      else if (ss == 'E') { waitT = 25000; }
+      else if (ss == 'F') { waitT = 20000; }
+      else if (ss == 'G') { waitT = 15000; }
+      else if (ss == 'H') { waitT = 13000; }
+      else if (ss == 'I') { waitT = 10000; }
+      else if (ss == 'J') { waitT = 8000; }
+      else if (ss == 'K') { waitT = 6000; }
+      else if (ss == 'L') { waitT = 5000; }
+      else if (ss == 'M') { waitT = 4000; }
+      else if (ss == 'N') { waitT = 3200; }
+      else if (ss == 'O') { waitT = 2500; }
+      else if (ss == 'P') { waitT = 2000; }
+      else if (ss == 'Q') { waitT = 1600; }
+      else if (ss == 'R') { waitT = 1300; }
+      else if (ss == 'S') { waitT = 1000; }
+      else if (ss == 'T') { waitT = 800; }
+      else if (ss == 'U') { waitT = 600; }
+      else if (ss == 'V') { waitT = 500; }
+      else if (ss == 'W') { waitT = 400; }
+      else if (ss == 'X') { waitT = 333; }
+      else if (ss == 'Y') { waitT = 250; }
+      else if (ss == 'a') { waitT = 200; }
+      else if (ss == 'b') { waitT = 167; }
+      else if (ss == 'c') { waitT = 125; }
+      else if (ss == 'd') { waitT = 100; }
+      else if (ss == 'e') { waitT = 77; }
+      else if (ss == 'f') { waitT = 67; }
+      else if (ss == 'g') { waitT = 50; }
+      else if (ss == 'h') { waitT = 40; }
+      else if (ss == 'i') { waitT = 33; }
+      else if (ss == 'j') { waitT = 25; }
+      else if (ss == 'k') { waitT = 20; }
+      else if (ss == 'l') { waitT = 17; }
+      else if (ss == 'm') { waitT = 13; }
+      else if (ss == 'n') { waitT = 10; }
+      else if (ss == 'o') { waitT = 8; }
+      
       startT = millis();
       onRun();
       ss = 'p';
       doubleSendl = 0;
+      
     }
+    
   }
-  nowT = millis();
   if (extra == 0) {
-    if ((nowT - startT) > waitT) {
+    if ((millis() - startT) > waitT) {
       offRun();
     }
   }
   else if (extra != 0) {
-    if ((nowT - startT) > 30000) {
+    if ((millis() - startT) > 30000) {
       extra = extra - 1;
       startT = millis();
     }
